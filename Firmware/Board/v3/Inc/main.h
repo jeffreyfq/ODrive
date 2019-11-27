94933f9db8251ce8be82c8f16ee5724eadfbc1b2
Jeffrey <jeffrey.queisser@oist.jp>
2019-11-27 10:14:13 +0900

    After MXCUBE regen

--- Firmware/Board/v3/Inc/main.h
+++ Firmware/Board/v3/Inc/main.h
@@ -1,58 +1,36 @@
+/* USER CODE BEGIN Header */
 /**
   ******************************************************************************
   * @file           : main.h
   * @brief          : Header for main.c file.
   *                   This file contains the common defines of the application.
   ******************************************************************************
-  * This notice applies to any and all portions of this file
-  * that are not between comment pairs USER CODE BEGIN and
-  * USER CODE END. Other portions of this file, whether 
-  * inserted by the user or by software development tools
-  * are owned by their respective copyright owners.
+  * @attention
   *
-  * Copyright (c) 2018 STMicroelectronics International N.V. 
-  * All rights reserved.
+  * <h2><center>&copy; Copyright (c) 2019 STMicroelectronics.
+  * All rights reserved.</center></h2>
   *
-  * Redistribution and use in source and binary forms, with or without 
-  * modification, are permitted, provided that the following conditions are met:
-  *
-  * 1. Redistribution of source code must retain the above copyright notice, 
-  *    this list of conditions and the following disclaimer.
-  * 2. Redistributions in binary form must reproduce the above copyright notice,
-  *    this list of conditions and the following disclaimer in the documentation
-  *    and/or other materials provided with the distribution.
-  * 3. Neither the name of STMicroelectronics nor the names of other 
-  *    contributors to this software may be used to endorse or promote products 
-  *    derived from this software without specific written permission.
-  * 4. This software, including modifications and/or derivative works of this 
-  *    software, must execute solely and exclusively on microcontroller or
-  *    microprocessor devices manufactured by or for STMicroelectronics.
-  * 5. Redistribution and use of this software other than as permitted under 
-  *    this license is void and will automatically terminate your rights under 
-  *    this license. 
-  *
-  * THIS SOFTWARE IS PROVIDED BY STMICROELECTRONICS AND CONTRIBUTORS "AS IS" 
-  * AND ANY EXPRESS, IMPLIED OR STATUTORY WARRANTIES, INCLUDING, BUT NOT 
-  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
-  * PARTICULAR PURPOSE AND NON-INFRINGEMENT OF THIRD PARTY INTELLECTUAL PROPERTY
-  * RIGHTS ARE DISCLAIMED TO THE FULLEST EXTENT PERMITTED BY LAW. IN NO EVENT 
-  * SHALL STMICROELECTRONICS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-  * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, 
-  * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
-  * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
-  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
-  * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  * This software component is licensed by ST under Ultimate Liberty license
+  * SLA0044, the "License"; You may not use this file except in compliance with
+  * the License. You may obtain a copy of the License at:
+  *                             www.st.com/SLA0044
   *
   ******************************************************************************
   */
+/* USER CODE END Header */
 
 /* Define to prevent recursive inclusion -------------------------------------*/
-#ifndef __MAIN_H__
-#define __MAIN_H__
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 /* Includes ------------------------------------------------------------------*/
+#include "stm32f4xx_hal.h"
 
+/* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include "stm32f4xx_hal.h"
 
@@ -65,7 +43,29 @@
 #else
 /* USER CODE END Includes */
 
-/* Private define ------------------------------------------------------------*/
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
 #define TIM_1_8_CLOCK_HZ 168000000
 #define TIM_1_8_PERIOD_CLOCKS 3500
 #define TIM_1_8_DEADTIME_CLOCKS 20
@@ -74,47 +74,30 @@
 #define TIM_APB1_DEADTIME_CLOCKS 40
 #define configAPPLICATION_ALLOCATED_HEAP 1
 #define TIM_1_8_RCR 2
-
 #define M0_nCS_Pin GPIO_PIN_13
 #define M0_nCS_GPIO_Port GPIOC
-#define M1_nCS_Pin GPIO_PIN_14
-#define M1_nCS_GPIO_Port GPIOC
-#define M1_ENC_Z_Pin GPIO_PIN_15
-#define M1_ENC_Z_GPIO_Port GPIOC
 #define M0_IB_Pin GPIO_PIN_0
 #define M0_IB_GPIO_Port GPIOC
 #define M0_IC_Pin GPIO_PIN_1
 #define M0_IC_GPIO_Port GPIOC
-#define M1_IC_Pin GPIO_PIN_2
-#define M1_IC_GPIO_Port GPIOC
-#define M1_IB_Pin GPIO_PIN_3
-#define M1_IB_GPIO_Port GPIOC
-#define GPIO_1_Pin GPIO_PIN_0
-#define GPIO_1_GPIO_Port GPIOA
-#define GPIO_2_Pin GPIO_PIN_1
-#define GPIO_2_GPIO_Port GPIOA
-#define GPIO_3_Pin GPIO_PIN_2
-#define GPIO_3_GPIO_Port GPIOA
-#define GPIO_4_Pin GPIO_PIN_3
-#define GPIO_4_GPIO_Port GPIOA
-#define M1_TEMP_Pin GPIO_PIN_4
-#define M1_TEMP_GPIO_Port GPIOA
-#define AUX_TEMP_Pin GPIO_PIN_5
-#define AUX_TEMP_GPIO_Port GPIOA
-#define VBUS_S_Pin GPIO_PIN_6
-#define VBUS_S_GPIO_Port GPIOA
-#define M1_AL_Pin GPIO_PIN_7
-#define M1_AL_GPIO_Port GPIOA
-#define GPIO_5_Pin GPIO_PIN_4
-#define GPIO_5_GPIO_Port GPIOC
+#define VBUS_S_Pin GPIO_PIN_2
+#define VBUS_S_GPIO_Port GPIOC
+#define MOTOR_TEMP_Pin GPIO_PIN_3
+#define MOTOR_TEMP_GPIO_Port GPIOC
+#define EXT_ANALOG_Pin GPIO_PIN_0
+#define EXT_ANALOG_GPIO_Port GPIOA
+#define LED1_Pin GPIO_PIN_2
+#define LED1_GPIO_Port GPIOA
+#define LED2_Pin GPIO_PIN_3
+#define LED2_GPIO_Port GPIOA
 #define M0_TEMP_Pin GPIO_PIN_5
 #define M0_TEMP_GPIO_Port GPIOC
-#define M1_BL_Pin GPIO_PIN_0
-#define M1_BL_GPIO_Port GPIOB
-#define M1_CL_Pin GPIO_PIN_1
-#define M1_CL_GPIO_Port GPIOB
-#define GPIO_6_Pin GPIO_PIN_2
-#define GPIO_6_GPIO_Port GPIOB
+#define EXT_PWM_Pin GPIO_PIN_0
+#define EXT_PWM_GPIO_Port GPIOB
+#define EXT_IO_Pin GPIO_PIN_1
+#define EXT_IO_GPIO_Port GPIOB
+#define BOOT1_Pin GPIO_PIN_2
+#define BOOT1_GPIO_Port GPIOB
 #define AUX_L_Pin GPIO_PIN_10
 #define AUX_L_GPIO_Port GPIOB
 #define AUX_H_Pin GPIO_PIN_11
@@ -127,42 +110,30 @@
 #define M0_BL_GPIO_Port GPIOB
 #define M0_CL_Pin GPIO_PIN_15
 #define M0_CL_GPIO_Port GPIOB
-#define M1_AH_Pin GPIO_PIN_6
-#define M1_AH_GPIO_Port GPIOC
-#define M1_BH_Pin GPIO_PIN_7
-#define M1_BH_GPIO_Port GPIOC
-#define M1_CH_Pin GPIO_PIN_8
-#define M1_CH_GPIO_Port GPIOC
-#define M0_ENC_Z_Pin GPIO_PIN_9
-#define M0_ENC_Z_GPIO_Port GPIOC
+#define PWM_FAN_Pin GPIO_PIN_8
+#define PWM_FAN_GPIO_Port GPIOC
+#define M0_DC_CAL_Pin GPIO_PIN_9
+#define M0_DC_CAL_GPIO_Port GPIOC
 #define M0_AH_Pin GPIO_PIN_8
 #define M0_AH_GPIO_Port GPIOA
 #define M0_BH_Pin GPIO_PIN_9
 #define M0_BH_GPIO_Port GPIOA
 #define M0_CH_Pin GPIO_PIN_10
 #define M0_CH_GPIO_Port GPIOA
-#define GPIO_7_Pin GPIO_PIN_15
-#define GPIO_7_GPIO_Port GPIOA
+#define SPI_NSS_DSPL_Pin GPIO_PIN_15
+#define SPI_NSS_DSPL_GPIO_Port GPIOA
 #define nFAULT_Pin GPIO_PIN_2
 #define nFAULT_GPIO_Port GPIOD
-#define GPIO_8_Pin GPIO_PIN_3
-#define GPIO_8_GPIO_Port GPIOB
-#define M0_ENC_A_Pin GPIO_PIN_4
-#define M0_ENC_A_GPIO_Port GPIOB
-#define M0_ENC_B_Pin GPIO_PIN_5
-#define M0_ENC_B_GPIO_Port GPIOB
-#define M1_ENC_A_Pin GPIO_PIN_6
-#define M1_ENC_A_GPIO_Port GPIOB
-#define M1_ENC_B_Pin GPIO_PIN_7
-#define M1_ENC_B_GPIO_Port GPIOB
-
-/* ########################## Assert Selection ############################## */
-/**
-  * @brief Uncomment the line below to expanse the "assert_param" macro in the 
-  *        HAL drivers code
-  */
-/* #define USE_FULL_ASSERT    1U */
-
+#define SPI_SCK_DSPL_Pin GPIO_PIN_3
+#define SPI_SCK_DSPL_GPIO_Port GPIOB
+#define SPI_MISO_DSPL_Pin GPIO_PIN_4
+#define SPI_MISO_DSPL_GPIO_Port GPIOB
+#define SPI_MOSI_DSPL_Pin GPIO_PIN_5
+#define SPI_MOSI_DSPL_GPIO_Port GPIOB
+#define RS424_TX_Pin GPIO_PIN_6
+#define RS424_TX_GPIO_Port GPIOB
+#define RS424_RX_Pin GPIO_PIN_7
+#define RS424_RX_GPIO_Port GPIOB
 /* USER CODE BEGIN Private defines */
 #endif
 
@@ -185,15 +156,9 @@
 /* USER CODE END Private defines */
 
 #ifdef __cplusplus
- extern "C" {
-#endif
-void _Error_Handler(char *, int);
-
-#define Error_Handler() _Error_Handler(__FILE__, __LINE__)
-#ifdef __cplusplus
 }
 #endif
 
-#endif /* __MAIN_H__ */
+#endif /* __MAIN_H */
 
 /************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
